adc5cdee0a40852f8893f83c50ca72327d8cf593
gekoJs <daniroa.js@gmail.com>
2023-05-11 22:47:39 -0400

    Finishing internatiolaitation

--- components/index.js
+++ components/index.js
@@ -3,4 +3,5 @@ export { default as Nav } from "./nav/Nav";
 export { default as Footer } from "./footer/Footer";
 export { default as Cursor } from "./cursor/Cursor";
 export { default as BurguerMenu } from "./burguerMenu/BurguerMenu";
-export {default as AnimatedPillar} from "./animatedPillar/AnimatedPillar"
+export { default as AnimatedPillar } from "./animatedPillar/AnimatedPillar";
+export { default as Translate } from "./translate/Translate";
